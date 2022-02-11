98d95432a940d5aac68c88f7b7c642ede2d39257
David Titarenco <david.titarenco@gmail.com>
2022-02-11 09:52:46 -0800

    boardgame

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,37 +1,55 @@
 import { immerable } from "immer"
+import type { Ctx, Game, Move } from "boardgame.io";
+import { DIMS } from "../constants";
 
-const P1: number = 1;
-const P2: number = 2;
-
-export enum CellState {
-  EMPTY = 0,
-  PLAYER_1_SHIP,
-  PLAYER_1_TERRITORY,
-  PLAYER_2_SHIP,
-  PLAYER_2_TERRITORY
+export interface AmazonsGameState {
+  cells: number[]
 }
 
-export class Game {
-  [immerable] = true
+const move: Move<AmazonsGameState> = (game, ctx, id) => {
+  game.cells[id] = Number(ctx.currentPlayer)
+};
 
-  dims: number;
-  turn: number = 0;
-  board: { state: CellState, coords: { x: number, y: number, z: number }}[];
-
-  constructor(dims: number) {
-    this.dims = dims;
-    // The board is a cube of volume dims³
-    // See: https://stackoverflow.com/questions/36781222/update-3d-matrix-with-coordinates-in-javascript
-    this.board =
-      [...Array(this.dims ** 3).keys()].map((n: number, idx: number) => {
-        const x = n % this.dims
-        const y = ((idx - x) / this.dims) % this.dims
-        const z = ((((idx - x) / this.dims) - y) / this.dims);
-        return { state: CellState.EMPTY, coords: { x, y, z } }
-      })
+export const Amazons: Game<AmazonsGameState> = {
+  setup: (ctx: Ctx, setupData) => {
+    return {
+      cells: Array(DIMS ** 3).fill(null)
+    }
   }
+};
 
-  public gameIsOver(): boolean {
-    return false
-  }
-}
+// const P1: number = 1;
+// const P2: number = 2;
+
+// export enum CellState {
+//   EMPTY = 0,
+//   PLAYER_1_SHIP,
+//   PLAYER_1_TERRITORY,
+//   PLAYER_2_SHIP,
+//   PLAYER_2_TERRITORY
+// }
+
+// export class Game {
+//   [immerable] = true
+
+//   dims: number;
+//   turn: number = 0;
+//   board: { state: CellState, coords: { x: number, y: number, z: number }}[];
+
+//   constructor(dims: number) {
+//     this.dims = dims;
+//     // The board is a cube of volume dims³
+//     // See: https://stackoverflow.com/questions/36781222/update-3d-matrix-with-coordinates-in-javascript
+//     this.board =
+//       [...Array(this.dims ** 3).keys()].map((n: number, idx: number) => {
+//         const x = n % this.dims
+//         const y = ((idx - x) / this.dims) % this.dims
+//         const z = ((((idx - x) / this.dims) - y) / this.dims);
+//         return { state: CellState.EMPTY, coords: { x, y, z } }
+//       })
+//   }
+
+//   public gameIsOver(): boolean {
+//     return false
+//   }
+// }
